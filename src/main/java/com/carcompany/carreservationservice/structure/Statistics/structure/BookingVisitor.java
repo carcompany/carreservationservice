d53cc704ddd64b517a0643e385626b2ab8007884
leoyn <leo@MacBook.local>
2020-08-30 17:45:23 +0200

    Add a method for each ExternalPaymentService

--- src/main/java/com/carcompany/carreservationservice/structure/Statistics/structure/BookingVisitor.java
+++ src/main/java/com/carcompany/carreservationservice/structure/Statistics/structure/BookingVisitor.java
@@ -1,5 +1,8 @@
 package com.carcompany.carreservationservice.structure.Statistics.structure;
 
 interface BookingVisitor {
-    void visit(PayPalExternalPaymentService paypal);
+    void visit(PayPalExternalPaymentService externalPaymentService);
+    void visit(ApplePayExternalPaymentService externalPaymentService);
+    void visit(GooglePayExternalPaymentService externalPaymentService);
+    void visit(BankExternalPaymentService externalPaymentService);
 }
--- src/main/java/com/carcompany/carreservationservice/structure/Statistics/structure/GermanBookingVisitor.java
+++ src/main/java/com/carcompany/carreservationservice/structure/Statistics/structure/GermanBookingVisitor.java
@@ -1,15 +1,13 @@
 package com.carcompany.carreservationservice.structure.Statistics.structure;
 
-import com.carcompany.carreservationservice.structure.Payment.structure.SacraficialPaymentProcess;
-
 /**
  * This class implements each operation declared by Visitor. Each operation
  * implements a fragment of the algorithm defined for the corresponding class of
  * object in the structure. ConcreteVisitor provides the context for the algorithm
  * and stores its local state. This state often accumulates results during the
  * traversal of the structure.
- * @author Sebastian
- * @version 1.0
+ * @author Sebastian, Kevin
+ * @version 1.1
  * @created 28-Aug-2020 17:10:47
  */
 public class GermanBookingVisitor implements BookingVisitor {
@@ -23,25 +21,33 @@ public class GermanBookingVisitor implements BookingVisitor {
 	}
 	/**
 	 * 
-	 * @param concreteElementA
+	 * @param externalPaymentService
 	 */
-	public void visit(PayPalExternalPaymentService concreteElementA){
+	public void visit(PayPalExternalPaymentService externalPaymentService) {
+
+	}
+	
+	/**
+	 * 
+	 * @param externalPaymentService
+	 */
+	public void visit(ApplePayExternalPaymentService externalPaymentService) {
 
 	}
 
 	/**
 	 * 
-	 * @param concreteElementB
+	 * @param externalPaymentService
 	 */
-	public void visit(GooglePayExternalPaymentService concreteElementB){
+	public void visit(GooglePayExternalPaymentService externalPaymentService) {
 
 	}
 
 	/**
 	 * 
-	 * @param MoneyWallet
+	 * @param externalPaymentService
 	 */
-	public void visit(SacraficialPaymentProcess MoneyWallet){
+	public void visit(BankExternalPaymentService externalPaymentService) {
 
 	}
 }//end GermanBookingVisitor
