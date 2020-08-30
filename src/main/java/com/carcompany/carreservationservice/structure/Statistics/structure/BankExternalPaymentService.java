680c04ef1d3ac3a84874db4ad8e5b381ade5d87b
leoyn <leo@MacBook.local>
2020-08-30 17:45:57 +0200

    Add BankExternalPaymentService

---
+++ src/main/java/com/carcompany/carreservationservice/structure/Statistics/structure/BankExternalPaymentService.java
@@ -0,0 +1,25 @@
+package com.carcompany.carreservationservice.structure.Statistics.structure;
+
+
+/**
+ * @author Kevin
+ * @version 1.0
+ * @created 28-Aug-2020 17:10:47
+ */
+public class BankExternalPaymentService implements ExternalPaymentService {
+
+	public BankExternalPaymentService(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	/**
+	 * 
+	 * @param bookingVisitor
+	 */
+	public void accept(BookingVisitor bookingVisitor){
+
+	}
+}//end BankExternalPaymentService
