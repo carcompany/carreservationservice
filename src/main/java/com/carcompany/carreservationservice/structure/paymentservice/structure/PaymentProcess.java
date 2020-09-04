411192dbca7dbb68dfff03656aa061a874ba35d0
Sebastian Meyne <sebastian.meyne@gmail.com>
2020-09-04 03:37:12 +0200

    Connected authentication system to payment system

--- src/main/java/com/carcompany/carreservationservice/structure/paymentservice/structure/PaymentProcess.java
+++ src/main/java/com/carcompany/carreservationservice/structure/paymentservice/structure/PaymentProcess.java
@@ -1,5 +1,8 @@
 package com.carcompany.carreservationservice.structure.paymentservice.structure;
 
+import com.carcompany.carreservationservice.structure.authenticationservice.behaviour.AuthenticationService;
+import com.carcompany.carreservationservice.structure.authenticationservice.behaviour.AuthenticationServiceImplementation;
+import com.carcompany.carreservationservice.structure.authenticationservice.structure.Role;
 import com.carcompany.carreservationservice.structure.personservice.structure.Person;
 
 /**
@@ -8,9 +11,12 @@ import com.carcompany.carreservationservice.structure.personservice.structure.Pe
  * @created 28-Aug-2020 17:10:49
  */
 public abstract class PaymentProcess {
+	 private AuthenticationService authenticationService;
 
 	public boolean authenticateCustomer(Person person) {
-		return false;
+		authenticationService = new AuthenticationServiceImplementation();
+		
+		return authenticationService.authenticateSubject(subject, credential, role);
 	}
 
 	public abstract void initiatePayment();
