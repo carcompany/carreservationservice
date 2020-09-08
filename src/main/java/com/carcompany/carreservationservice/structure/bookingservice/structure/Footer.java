13a84141d4420c126d185f8b3dd1fb9ca701a4cd
Sebastian Meyne <sebastian.meyne@gmail.com>
2020-09-08 22:16:47 +0200

    Implemented Payment property

--- src/main/java/com/carcompany/carreservationservice/structure/bookingservice/structure/Footer.java
+++ src/main/java/com/carcompany/carreservationservice/structure/bookingservice/structure/Footer.java
@@ -1,5 +1,6 @@
 package com.carcompany.carreservationservice.structure.bookingservice.structure;
 
+import com.carcompany.carreservationservice.structure.paymentservice.structure.Payment;
 
 /**
  * @author Kevin
@@ -7,5 +8,23 @@ package com.carcompany.carreservationservice.structure.bookingservice.structure;
  * @created 28-Aug-2020 17:10:46
  */
 public class Footer {
+
+    private Payment payment;
+
+
+    public Footer(){
+
+
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    
 
 }
